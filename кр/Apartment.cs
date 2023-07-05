a638cb279d250f9a1417857badaa6b95c459ede2
amirtanasov@gmail.com <amirtanasov@gmail.com>
2023-07-05 10:24:26 +0300

    Добавьте файлы проекта.

---
+++ кр/Apartment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLibrary
+{
+    public class Apartment
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public List<Room> Rooms { get; set; }
+
+        public Apartment(int number, string owner) //Студент Танашов Амир
+        {
+            Number = number;
+            Owner = owner;
+            Rooms = new List<Room>();
+        }
+
+        public void AddRoom(Room room)
+        {
+            Rooms.Add(room);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Apartment: Number={Number}, Owner={Owner}");
+        }
+
+        public void ShowAll()
+        {
+            foreach (Room room in Rooms)//Студент Танашов Амир
+            {
+                room.Show();
+            }
+        }
+    }
+}
---
+++ кр/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Room : IComparable<Room>
+    {
+        public string Name { get; set; }
+        public double Area { get; set; }
+
+        public Room(string name, double area) //Студент Танашов Амир
+        {
+            Name = name;
+            Area = area;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Room: Name={Name}, Area={Area}");
+        }
+
+        public int CompareTo(Room other)
+        {1xz
+            return Area.CompareTo(other.Area);//Студент Танашов Амир
+        }
+    }
+}
